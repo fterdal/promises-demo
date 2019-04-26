91e048ae3b826248713de38dd5e29057434ac2a8
Finn <finnterdal@gmail.com>
2019-04-26 10:20:07 -0500

    async demo

--- promises.js
+++ promises.js
@@ -1,2 +1,20 @@
 const { waitForSuccess, waitForFailure } = require('./waits')
 
+const hello = async (string) => {
+  await waitForFailure(1000)
+  return string
+}
+
+async function waitForIt() {
+  try {
+    const string = await hello('hello')
+    console.log('IT WORKED!', string)
+  } catch (err) {
+    console.log('OH NO!', err)
+  }
+}
+waitForIt()
+// console.log('hello returns',
+//   hello('hello')
+//   .then(str => console.log(str))
+// )
