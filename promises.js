bdd48cbcd1cec207a2a8d1cccc91964780159c95
Finn <finnterdal@gmail.com>
2019-04-26 10:36:25 -0500

    finish promises demo for 1904

--- promises.js
+++ promises.js
@@ -1,19 +1,37 @@
 const { waitForSuccess, waitForFailure } = require('./waits')
 
 const hello = async (string) => {
-  await waitForFailure(1000)
+  await waitForSuccess(1000)
   return string
 }
 
-async function waitForIt() {
-  try {
-    const string = await hello('hello')
-    console.log('IT WORKED!', string)
-  } catch (err) {
-    console.log('OH NO!', err)
+const arr = ['finn', 'collin', 'priti']
+
+async function sayHelloToInstructorsParallel() {
+  const instructors = await Promise.all(
+    arr.map(instructor => hello(instructor))
+  )
+  console.log('instructors', instructors)
+  console.log('Theyre all done!')
+}
+sayHelloToInstructorsParallel()
+async function sayHelloToInstructors() {
+  for (let i = 0; i < arr.length; i++) {
+    const name = await hello(arr[i])
+    console.log(name)
   }
 }
-waitForIt()
+// sayHelloToInstructors()
+
+// async function waitForIt() {
+//   try {
+//     const string = await hello('hello')
+//     console.log('IT WORKED!', string)
+//   } catch (err) {
+//     console.log('OH NO!', err)
+//   }
+// }
+// waitForIt()
 // console.log('hello returns',
 //   hello('hello')
 //   .then(str => console.log(str))
